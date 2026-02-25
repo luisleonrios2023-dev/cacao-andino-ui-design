2d920ec40b112b6b9ebf08f9165f2d6a87554b2b
Luis <luisleonrios2023@gmail.com>
2026-02-25 18:05:28 +0100

    Función abrir y cerrar menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
+// Inyecta el header.html
 fetch("partials/header.html")
-  .then(response => response.txt())
-  .then(data => {
-    document.getElementById("header-container").innerHTML = data;
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("No se pudo cargar el header");
+    }
+    return response.text();
   })
-  .catch(error => console.error("Error cargando el header"));
+  .then(html => {
+    document.getElementById("header-container").innerHTML = html;
+
+    //Aquí ya existe el html header
+    const icono_menu = document.getElementById("icono-menu");
+    const menu = document.getElementById("menu");
+
+    icono_menu.addEventListener('click', () => {
+      menu.classList.toggle('menu-open');
+    })
+  })
+  .catch(error => {
+    console.error("Error cargando el header:", error);
+  });
+
+
+
+
